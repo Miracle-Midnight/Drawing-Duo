24cb5864c26ca4a5d48b439ff0fc95ead72f661d
young970 <yyww9@naver.com>
2023-01-04 20:04:31 +0900

    Feat: Add 게임 대기실 진입 구현
    게임 대기실 진입 구현함 uri: post lobby/{roomid}

--- backend/src/lobby/lobby.controller.ts
+++ backend/src/lobby/lobby.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Render } from '@nestjs/common';
+import { LobbyService } from './lobby.service';
 
 @Controller('lobby')
 export class LobbyController {
+  constructor(private readonly lobbyService: LobbyService) {}
   @Get()
   @Render('lobby')
   async GetLobby(): Promise<void> {
     return;
+  }
+
+  @Post(':id')
+  inRoom(@Body() userid, @Param('id') roomid: string) {
+    return this.lobbyService.inRoom(userid, +roomid);
   }
 }
--- backend/src/lobby/lobby.module.ts
+++ backend/src/lobby/lobby.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Room } from 'src/room/entities/room.entity';
+import { User } from 'src/user/entities/user.entity';
 import { LobbyController } from './lobby.controller';
 import { LobbyService } from './lobby.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Room])],
   controllers: [LobbyController],
   providers: [LobbyService],
 })
