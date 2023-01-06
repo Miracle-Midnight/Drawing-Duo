bfbcd3d9361df2bc49e891907af50d9e55b2a90c
kimseungdeok <tmdejr1117@gmail.com>
2023-01-07 04:32:41 +0900

    Feat : 게임 로비 나가기 버튼 추가

--- frontend/src/routes/gameLobby/gameLobby.tsx
+++ frontend/src/routes/gameLobby/gameLobby.tsx
@@ -100,9 +100,17 @@ function Login() {
             </div>
           </div>
         </div>
-        <div>
-          <div className="chat mx-auto rounded-md w-full h-full">
-            <div className="container flex flex-col items-center justify-center w-full mx-auto pt-11 pb-11">
+        <div className="flex flex-col justify-between">
+          <div className="ml-64">
+            <button
+              className="px-4 w-30 py-2 ml-5 mb-3 text-base font-semibold text-white bg-gray-600 rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-purple-200 justify-center place-self-center"
+              type="submit"
+            >
+              나가기
+            </button>
+          </div>
+          <div className="chat mx-auto rounded-md w-full">
+            <div className="container flex flex-col items-center justify-center w-full mx-auto">
               <ul className="flex flex-col overflow-auto chat-container w-full">
                 <li className="flex flex-row mb-2 border-gray-400">
                   <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
@@ -186,12 +194,12 @@ function Login() {
 
               <div className="w-full rounded-md">
                 <div className="text-center font-light flex items-center justify-center">
-                  <form className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
+                  <form className="flex flex-col justify-center w-full max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
                     <div className=" relative ">
                       <input
                         type="text"
                         id="form-subscribe-Subscribe"
-                        className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-12 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                         placeholder="채팅"
                       />
                     </div>
@@ -206,13 +214,15 @@ function Login() {
               </div>
             </div>
           </div>
-          <div>
-            <button
-              className="px-4 w-full py-2 ml-10 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200 justify-center place-self-center"
-              type="submit"
-            >
-              Ready
-            </button>
+          <div className="pt-10">
+            <div>
+              <button
+                className="px-4 w-96 py-2 ml-5 mb-6 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200 justify-center place-self-center"
+                type="submit"
+              >
+                Ready
+              </button>
+            </div>
           </div>
         </div>
       </Container>
