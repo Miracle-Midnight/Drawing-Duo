637cc3bdecb8a2545da5741d425ad78d9dfb22c7
kcw2297 <chaeugg@gmail.com>
2023-01-07 00:14:12 +0900

    Youtube: nono text

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import * as awarenessProtocol from "y-protocols/awareness.js";
+// Run : PORT=4444 node ./bin/server.js
+
+const doc = new Y.Doc();
+const provider = new WebrtcProvider("test", doc, {
+  // Specify signaling servers. The client will connect to every signaling server concurrently to find other peers as fast as possible.
+  signaling: [
+    "wss://signaling.yjs.dev",
+    "wss://y-webrtc-signaling-eu.herokuapp.com",
+    "wss://y-webrtc-signaling-us.herokuapp.com",
+  ],
+  password: null,
+  awareness: new awarenessProtocol.Awareness(doc),
+  maxConns: 20 + Math.floor(Math.random() * 15),
+  filterBcConns: false, // true: only connect to peers that are in the same room
+  peerOpts: {},
+}); // (방이름,doc), 동기화 연결을 위한 provider로 webrtc 사용
+
+// const yary = doc.get("array", Y.Array); // doc에 array라는 이름의 Y.Array를 가져옴
+const yary = doc.getArray("array"); // doc에 array라는 이름의 Y.Array를 가져옴 (위와 동일?), 이곳에 공유된 데이터를 저장한다
+
+provider.on("synced", (synced: any) => {
+  console.log(synced);
+  // console.log(`Synced! ${synced}`); // 동기화 완료, synced => object { synced: true }
+});
+
+yary.observeDeep((event) => {
+  console.log(event);
+  console.log(`yary updated: ${yary.toJSON()}`);
+});
+
+yary.insert(0, ["nono"]); // 1번 idx에 nono 삽입
+
+const aw = provider.awareness; // awareness는 provider에 포함되어 있음
+aw.on("change", (changes: any) => {
+  console.log(Array.from(aw.getStates().entries()));
+});
+
+aw.setLocalStateField("user", { name: "kim", color: "#ffb61e" });
 
 function App() {
+  // console.log(doc);
+  // console.log(yary);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>Hi!</p>
+        <p>{yary.toJSON()}</p>
+        <div
+          onClick={() => yary.insert(0, ["test"])}
+          style={{ cursor: "pointer" }}
         >
-          Learn React
-        </a>
+          Click me!
+        </div>
       </header>
     </div>
   );
---
+++ temp.tsx
@@ -0,0 +1,61 @@
+// import { useCallback } from "react";
+// import { useSelf, useUsers } from "y-presence";
+// import { WebsocketProvider } from "y-websocket";
+// import { Doc } from "yjs";
+
+// const doc = new Doc();
+
+// // Create a websocket provider
+// const provider = new WebsocketProvider(
+//   "wss://demos.yjs.dev",
+//   "example-react",
+//   doc
+// );
+
+// const awareness = provider.awareness;
+
+// awareness.setLocalState({ name: "John Doe", email: "johndoe@gmail.com" });
+
+// export default function App() {
+//   return (
+//     <>
+//       <All />
+//       <Self />
+//     </>
+//   );
+// }
+
+// function All() {
+//   const users = useUsers(awareness);
+
+//   return (
+//     <div>
+//       <h2>All Users: {users.size}</h2>
+//       {Array.from(users.entries()).map(([key, value]) => {
+//         return <div key={key}>{value.name}</div>;
+//       })}
+//     </div>
+//   );
+// }
+
+// function Self() {
+//   const state = useSelf(awareness, (state: any) => {
+//     if (state) {
+//       return state.name as string;
+//     }
+//   });
+
+//   const handleChange = useCallback(
+//     (event: React.ChangeEvent<HTMLInputElement>) => {
+//       awareness.setLocalStateField("name", event.target.value);
+//     },
+//     []
+//   );
+
+//   return (
+//     <div>
+//       <h2>Self: {awareness.clientID}</h2>
+//       <input value={state ?? ""} onChange={handleChange} />
+//     </div>
+//   );
+// }
