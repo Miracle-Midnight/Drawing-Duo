c8a97660d63aae454ff0ebc6d11a1b9b7d719406
kimseungdeok <tmdejr1117@gmail.com>
2023-01-06 22:04:53 +0900

    Feat : 로그인 페이지 생성

--- frontend/src/routes/register/register.tsx
+++ frontend/src/routes/register/register.tsx
@@ -82,7 +82,7 @@ function Register() {
               </button>
               <button
                 type="submit"
-                className="py-2 px-4 ml-2 bg-blue-700 hover:bg-blue-500 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                className="py-2 px-4 ml-2 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
               >
                 회원 가입
               </button>
