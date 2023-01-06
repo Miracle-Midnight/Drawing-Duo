4d7b3560dc63f22008c3abcfe2ac495376343347
kimseungdeok <tmdejr1117@gmail.com>
2023-01-06 17:01:50 +0900

    Style : 버튼 색 수정

--- frontend/src/routes/register/register.tsx
+++ frontend/src/routes/register/register.tsx
@@ -76,7 +76,7 @@ function Login() {
             <div className="flex w-full my-4">
               <button
                 type="submit"
-                className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                className="py-2 px-4  bg-blue-700 hover:bg-blue-500 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
               >
                 Login
               </button>
