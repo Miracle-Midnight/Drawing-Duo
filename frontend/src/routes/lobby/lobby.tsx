2d8e9000a9bb7f406d3bb735b0a1bec104c3abc1
kimseungdeok <tmdejr1117@gmail.com>
2023-01-07 17:16:23 +0900

    Fix : 모달 창 내용 추가
    공개 여부와 비밀번호 추가 및 css 변경

--- frontend/src/routes/lobby/lobby.tsx
+++ frontend/src/routes/lobby/lobby.tsx
@@ -208,20 +208,50 @@ function Lobby() {
                   <span className="text-red-500 required-dot">*</span>
                 </div>
 
-                <div className="flex items-center">
+                <div className="flex w-full my-1">
                   <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base font-medium text-black bg-white border border-b border-l rounded-l-md hover:bg-purple-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
-                  >
-                    랜덤 모드
-                  </button>
-
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base font-medium text-black bg-white border-t border-b border-r rounded-r-md hover:bg-purple-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
+                    type="submit"
+                    className="py-2 px-4 ml-2 border border-gray-300 bg-white hover:bg-purple-700 focus:ring-purple-700 focus:bg-purple-700 focus:ring-offset-purple-200 text-black w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                   >
                     픽 모드
                   </button>
+                  <button
+                    type="submit"
+                    className="py-2 px-4 ml-2 border border-gray-300 bg-white hover:bg-purple-700 focus:ring-purple-700 focus:bg-purple-700 focus:ring-offset-purple-200 text-black w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  >
+                    랜덤 모드
+                  </button>
+                </div>
+                <div>
+                  공개 여부
+                  <span className="text-red-500 required-dot">*</span>
+                </div>
+
+                <div className="flex w-full my-1">
+                  <button
+                    type="submit"
+                    className="py-2 px-4 ml-2 border border-gray-300 bg-white hover:bg-purple-700 focus:ring-purple-700 focus:bg-purple-700 focus:ring-offset-purple-200 text-black w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  >
+                    공개
+                  </button>
+                  <button
+                    type="submit"
+                    className="py-2 px-4 ml-2 border border-gray-300 bg-white hover:bg-purple-700 focus:ring-purple-700 focus:bg-purple-700 focus:ring-offset-purple-200 text-black w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  >
+                    비공개
+                  </button>
+                </div>
+                <div className=" relative mb-3">
+                  <label className="text-gray-700">
+                    비밀번호
+                    <span className="text-red-500 required-dot">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    name="room"
+                    placeholder="비밀번호 입력"
+                  />
                 </div>
                 <div className="mt-3">사진</div>
                 <InputGroup>
@@ -235,13 +265,22 @@ function Lobby() {
                   </Button>
                 </InputGroup>
               </Modal.Body>
-              <Modal.Footer className="">
-                <Button variant="secondary" onClick={handleClose}>
+              <Modal.Footer className="flex">
+                <button
+                  type="button"
+                  className="py-2 px-4 text-white rounded-lg bg-gray-500 justify-center place-self-center"
+                  onClick={handleClose}
+                >
                   취소
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
+                </button>
+
+                <button
+                  type="button"
+                  className="py-2 px-4 text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200 justify-center place-self-center"
+                  onClick={handleClose}
+                >
                   방 만들기
-                </Button>
+                </button>
               </Modal.Footer>
             </Modal>
           </div>
