0c377850ef5a155081ac34e47f37ae7258e7898a
kcw2297 <chaeugg@gmail.com>
2023-01-08 01:48:47 +0900

    Refactor : Fix moving drawing-component's logic into separate file(before)
    drawing-componment 파일안에 있는 비즈니스 로직을 yjs-pfreehand 파일 내부로 옮김

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
+/* react */
 import "./App.css";
-
-import * as Y from "yjs";
-import { DrawLine, updateLine } from "./components/drawing/drawing-component";
-
-/*crdt document 생성 */
-const ydoc = new Y.Doc();
-const ystrokes = ydoc.getArray("stroke");
-ystrokes.observe((event: any) => {
-  updateLine(ystrokes);
-});
-
-const coords = [
-  [0, 0, 0],
-  [100, 340, 0.5],
-  [400, 300, 0.5],
-  [500, 600, 0.5],
-];
-const newStorke = new Y.Array();
-newStorke.push(coords); // newStorke.toArray() == coords
-ystrokes.push([newStorke]);
+/* module from local */
+import { DrawLine } from "./components/drawing/drawing-component";
 
 /* App 컴포넌트 */
 function App() {
@@ -26,7 +9,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hi!</p>
-        <DrawLine ystrokes={ystrokes} />
+        <svg className={"canvas"} width="400" height="210"></svg>
+        <DrawLine />
       </header>
     </div>
   );
--- src/components/drawing/drawing-component.tsx
+++ src/components/drawing/drawing-component.tsx
@@ -1,45 +1,78 @@
+/* react */
 import { useEffect } from "react";
+/* library */
 import { getStroke } from "perfect-freehand";
+import * as Y from "yjs";
+/* module from local */
 import { getSvgPathFromStroke } from "../../utils/drawing/drawing.utils";
 
-export function _newPath(ystrokes: any, svg: SVGSVGElement) {
-  console.log("newPath");
+/*crdt document 생성*/
+const ydoc = new Y.Doc();
+export const ystrokes = ydoc.getArray("stroke");
+
+const coords = [
+  [0, 0, 0],
+  [100, 340, 0.5],
+  [400, 300, 0.5],
+  [500, 600, 0.5],
+];
+
+const newStorke = new Y.Array();
+newStorke.push(coords); // newStorke.toArray() == coords
+ystrokes.push([newStorke]);
+
+/* 전역 변수 생성 */
+let currentStroke: any = null;
+
+export function eventStorke(ystrokes: any, svg: any) {
+  svg?.addEventListener("pointerdown", (event: any) => {
+    currentStroke = new Y.Array();
+    currentStroke.push([[event.x, event.y, event.pressure]]);
+    ystrokes.push([currentStroke]);
+  });
+  svg?.addEventListener("pointermove", (event: any) => {
+    if (event.button !== 1) {
+      currentStroke = null;
+      return;
+    }
+    currentStroke.push([[event.x, event.y, event.pressure]]);
+  });
+}
+
+export function _updatePaths(ystrokes: any, svg: any) {
   ystrokes.forEach((ystroke: any) => {
     const stroke = getStroke(ystroke.toArray());
     const svgPath = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "path"
     );
+
     svgPath.setAttribute("d", getSvgPathFromStroke(stroke));
     svg.appendChild(svgPath);
-    console.log(svg);
   });
 }
 
-export function updateLine(ystrokes: any) {
-  const svg = document.querySelector("svg");
-  while (svg?.firstChild) {
-    svg.removeChild(svg.firstChild);
-  }
+export function _removePaths(svg: Element): void {
   if (svg) {
-    _newPath(ystrokes, svg);
+    while (svg?.firstChild) {
+      svg.removeChild(svg.firstChild);
+    }
   }
+  return;
 }
 
-export function renderLine(ystrokes: any) {
-  console.log("renderLine");
-  const svg = document.querySelector("svg");
-  if (svg) {
-    _newPath(ystrokes, svg);
-  }
-}
-
-export function DrawLine(props: any) {
-  console.log("drawline");
+export function DrawLine() {
   useEffect(() => {
-    renderLine(props.ystrokes);
-  }, [props.ystrokes]);
+    const svg = document.getElementsByClassName("canvas")[0];
+    if (svg) {
+      _updatePaths(ystrokes, svg);
+      ystrokes.observe((event: any) => {
+        _removePaths(svg);
+        _updatePaths(ystrokes, svg);
+      });
+    }
+  }, []);
 
-  return <svg width="400" height="210"></svg>;
+  return <span></span>; // component로 사용하기 위해 빈 태그 반환
 }
 export default DrawLine;
