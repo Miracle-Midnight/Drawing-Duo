c47309a51d62ffb83e3d754a690b86dd3a6200e4
kcw2297 <chaeugg@gmail.com>
2023-01-07 13:08:56 +0900

    Collaborative Drawing App: 43m, draw a single line

--- src/App.tsx
+++ src/App.tsx
@@ -2,64 +2,42 @@ import React from "react";
 import "./App.css";
 
 import * as Y from "yjs";
-import { WebrtcProvider } from "y-webrtc";
-import * as awarenessProtocol from "y-protocols/awareness.js";
-// Run : PORT=4444 node ./bin/server.js
+import { getStroke } from "perfect-freehand";
+import { getSvgPathFromStroke } from "./utils/drawing/drawing.utils";
 
-const doc = new Y.Doc();
-const provider = new WebrtcProvider("test", doc, {
-  // Specify signaling servers. The client will connect to every signaling server concurrently to find other peers as fast as possible.
-  signaling: [
-    "wss://signaling.yjs.dev",
-    "wss://y-webrtc-signaling-eu.herokuapp.com",
-    "wss://y-webrtc-signaling-us.herokuapp.com",
-  ],
-  password: null,
-  awareness: new awarenessProtocol.Awareness(doc),
-  maxConns: 20 + Math.floor(Math.random() * 15),
-  filterBcConns: false, // true: only connect to peers that are in the same room
-  peerOpts: {},
-}); // (방이름,doc), 동기화 연결을 위한 provider로 webrtc 사용
+const ydoc = new Y.Doc();
+const ystrokes = ydoc.getArray("stroke");
 
-// const yary = doc.get("array", Y.Array); // doc에 array라는 이름의 Y.Array를 가져옴
-const yary = doc.getArray("array"); // doc에 array라는 이름의 Y.Array를 가져옴 (위와 동일?), 이곳에 공유된 데이터를 저장한다
+const coords = [
+  [0, 0, 0],
+  [100, 100, 0.5],
+  [300, 300, 0.5],
+  [600, 600, 0.5],
+];
+const newStorke = new Y.Array();
 
-provider.on("synced", (synced: any) => {
-  console.log(synced);
-  // console.log(`Synced! ${synced}`); // 동기화 완료, synced => object { synced: true }
-});
+newStorke.push(coords);
+ystrokes.push([newStorke]);
 
-yary.observeDeep((event) => {
-  console.log(event);
-  console.log(`yary updated: ${yary.toJSON()}`);
-});
+// console.log(ystrokes.toJSON());
+// console.log(newStorke.toJSON());
+const stroke = getStroke(coords);
+// console.log(stroke);
 
-yary.insert(0, ["nono"]); // 1번 idx에 nono 삽입
+// 강의 밑에 함수는 getSvgPathFromStroke 정의와 svgPath를 만든 후 svg에 추가하는 부분이다.
 
-const aw = provider.awareness; // awareness는 provider에 포함되어 있음
-aw.on("change", (changes: any) => {
-  console.log(Array.from(aw.getStates().entries()));
-});
-
-aw.setLocalStateField("user", { name: "kim", color: "#ffb61e" });
+console.log(getSvgPathFromStroke(stroke));
 
 function App() {
-  // console.log(doc);
-  // console.log(yary);
   return (
     <div className="App">
       <header className="App-header">
         <p>Hi!</p>
-        <p>{yary.toJSON()}</p>
-        <div
-          onClick={() => yary.insert(0, ["test"])}
-          style={{ cursor: "pointer" }}
-        >
-          Click me!
-        </div>
+        <svg width="400" height="210">
+          <path d={getSvgPathFromStroke(stroke)} />
+        </svg>
       </header>
     </div>
   );
 }
-
 export default App;
--- temp.tsx
+++ temp.tsx
@@ -1,61 +1,178 @@
-// import { useCallback } from "react";
-// import { useSelf, useUsers } from "y-presence";
-// import { WebsocketProvider } from "y-websocket";
-// import { Doc } from "yjs";
+// Html
+// <!DOCTYPE html>
+// <html>
+//   <head>
+//     <meta charset="utf-8">
+//     <meta name="viewport" content="width=device-width">
+//     <title>repl.it</title>
+//     <link href="style.css" rel="stylesheet" type="text/css" />
+//   </head>
+//   <body>
+//     <script src="script.js" type="module"></script>
+//     <svg xmlns="http://www.w3.org/2000/svg"></svg>
+//   </body>
+// </html>
 
-// const doc = new Doc();
-
-// // Create a websocket provider
-// const provider = new WebsocketProvider(
-//   "wss://demos.yjs.dev",
-//   "example-react",
-//   doc
-// );
-
-// const awareness = provider.awareness;
-
-// awareness.setLocalState({ name: "John Doe", email: "johndoe@gmail.com" });
-
-// export default function App() {
-//   return (
-//     <>
-//       <All />
-//       <Self />
-//     </>
-//   );
+// CSS
+// html,
+// * {
+//   box-sizing: border-box;
+//   padding: 0;
+//   margin: 0;
 // }
 
-// function All() {
-//   const users = useUsers(awareness);
-
-//   return (
-//     <div>
-//       <h2>All Users: {users.size}</h2>
-//       {Array.from(users.entries()).map(([key, value]) => {
-//         return <div key={key}>{value.name}</div>;
-//       })}
-//     </div>
-//   );
+// body {
+//   user-select: none;
 // }
 
-// function Self() {
-//   const state = useSelf(awareness, (state: any) => {
-//     if (state) {
-//       return state.name as string;
+// svg {
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
+//   background-color: #f8efef;
+//   touch-action: none;
+// }
+
+// JS
+
+// import { getStroke } from 'https://esm.sh/perfect-freehand@1'
+// import * as Y from 'https://esm.sh/yjs@13'
+// import { WebrtcProvider } from 'https://esm.sh/y-webrtc@10'
+
+// const ydoc = new Y.Doc()
+// const provider = new WebrtcProvider('awesome drawing', ydoc, { password: 'test' })
+
+// const svg = document.querySelector('svg')
+
+// const oneOf = arr => arr[Math.floor(Math.random() * arr.length)]
+
+// const colors = [
+//   { color: '#30bced', light: '#30bced33' },
+//   { color: '#6eeb83', light: '#6eeb8333' },
+//   { color: '#ffbc42', light: '#ffbc4233' },
+//   { color: '#ecd444', light: '#ecd44433' },
+//   { color: '#ee6352', light: '#ee635233' },
+//   { color: '#9ac2c9', light: '#9ac2c933' },
+//   { color: '#8acb88', light: '#8acb8833' },
+//   { color: '#1be7ff', light: '#1be7ff33' }
+// ]
+
+// const names = ['Alice', 'Bob', 'Stephen', 'Francois']
+
+// const awareness = provider.awareness
+
+// awareness.setLocalStateField('user', { name: oneOf(names), color: oneOf(colors).color })
+
+// awareness.on('change', event => {
+//   svg.querySelectorAll('circle').forEach(circle => circle.remove())
+//   awareness.getStates().forEach((state, clientID) => {
+//     if (clientID === awareness.clientID) {
+//       return
 //     }
-//   });
+//     const pos = state.pos
+//     if (!pos) {
+//       return
+//     }
+//     const svgCircle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+//     //   <circle cx="50" cy="50" r="50"/>
+//     svgCircle.setAttribute('cx', pos.x)
+//     svgCircle.setAttribute('cy', pos.y)
+//     svgCircle.setAttribute('r', 10)
+//     svgCircle.setAttribute('fill', state.user.color)
+//     svg.appendChild(svgCircle)
+//   })
+// })
 
-//   const handleChange = useCallback(
-//     (event: React.ChangeEvent<HTMLInputElement>) => {
-//       awareness.setLocalStateField("name", event.target.value);
+// const getSvgPathFromStroke = stroke => {
+//   if (!stroke.length) return ""
+
+//   const d = stroke.reduce(
+//     (acc, [x0, y0], i, arr) => {
+//       const [x1, y1] = arr[(i + 1) % arr.length]
+//       acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2)
+//       return acc
 //     },
-//     []
-//   );
+//     ["M", ...stroke[0], "Q"]
+//   )
 
-//   return (
-//     <div>
-//       <h2>Self: {awareness.clientID}</h2>
-//       <input value={state ?? ""} onChange={handleChange} />
-//     </div>
-//   );
+//   d.push("Z")
+//   return d.join(" ")
 // }
+
+// const ystrokes = ydoc.getArray('strokes')
+
+// const strokeStyle = {
+//   size: 12,
+//   thinning: 0.5,
+//   smoothing: 0.5,
+//   streamline: 0.5,
+//   easing: (t) => t,
+//   start: {
+//     taper: 0,
+//     easing: (t) => t,
+//     cap: true
+//   },
+//   end: {
+//     taper: 100,
+//     easing: (t) => t,
+//     cap: true
+//   }
+// };
+
+// const getSvgStrokeFromYStroke = ystroke => getSvgPathFromStroke(getStroke(ystroke.get('path').toArray()), strokeStyle)
+
+// ystrokes.observe(event => {
+//   event.changes.added.forEach(item => {
+//     item.content.getContent().forEach(ystroke => {
+//       const svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+//       svgPath.setAttribute('d', getSvgStrokeFromYStroke(ystroke))
+//       svgPath.setAttribute('fill', ystroke.get('color'))
+//       svg.appendChild(svgPath)
+//       ystroke.get('path').observe(event => {
+//         svgPath.setAttribute('d', getSvgStrokeFromYStroke(ystroke))
+//       })
+//       ystroke.observe(event => {
+//         if (event.keysChanged.has('color')) {
+//           svgPath.setAttribute('fill', ystroke.get('color'))
+//         }
+//       })
+//     })
+//   })
+// })
+
+// /**
+//  * @type {Y.Map<> | null}
+//  */
+// let currentStroke = null
+// svg.addEventListener('pointerdown', event => {
+//   currentStroke = new Y.Map()
+//   currentStroke.set('color', oneOf(colors).color)
+//   const currentPath = new Y.Array()
+//   currentPath.push([[ event.x, event.y, event.pressure ]])
+//   currentStroke.set('path', currentPath)
+//   ystrokes.push([currentStroke])
+// })
+
+// svg.addEventListener('pointermove', event => {
+//   awareness.setLocalStateField('pos', { x: event.x, y: event.y })
+//   if (event.buttons !== 1) {
+//     currentStroke = null
+//     return
+//   }
+//   currentStroke.get('path').push([[ event.x, event.y, event.pressure]])
+// })
+
+// window.Y = Y
+// window.svg = svg
+// window.ystrokes = ystrokes
+// window.ydoc = ydoc
+
+// /**
+//  * @type {Y.Array<Y.Map<>>}
+//  * ymap.set('pos', {x, y})
+//  *
+//  * ymap.set('posy', y)
+//  *
+//  */
