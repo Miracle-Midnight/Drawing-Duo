5c776981d4b38ec363684e5038588a68c967fe41
kcw2297 <chaeugg@gmail.com>
2023-01-09 00:34:49 +0900

    Refactor: Fix react migrate[Debug]
    react로 옮긴 후 디버깅 필요

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 /* react */
 import "./App.css";
 /* module from local */
+import { Canvas } from "./components/Canvas/canvas";
 
 /* App 컴포넌트 */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hi!</p>
-      </header>
+      <Canvas />
     </div>
   );
 }
--- src/components/Canvas/canvas.tsx
+++ src/components/Canvas/canvas.tsx
@@ -1,6 +1,5 @@
 /* library */
 import { useCallback } from "react";
-import * as Y from "yjs";
 /* module from local */
 import { Line } from "../Line/line";
 import { useLines } from "../../hooks/useLines";
@@ -22,17 +21,66 @@ function getPoint(x: number, y: number) {
 }
 
 /* 화면에 보일 캔버스 그림 정보 */
-export default function Canvas() {
-  const { lines, startLine, addPointToLine, completeLine, clearAllLines } =
-    useLines();
+export function Canvas() {
+  const { lines, startLine, addPointToLine, completeLine } = useLines();
 
   /* 포인터가 눌러지면, 새로운 라인을 시작 */
   const handlePointerDown = useCallback(
     (e: React.PointerEvent<SVGSVGElement>) => {
       e.currentTarget.setPointerCapture(e.pointerId);
 
-      startLine(getPoint(e.clientX, e.clientY));
+      startLine(getPoint(e.clientX, e.clientY)); // yLines에 추가되면서, curRef상태 변환
     },
     [startLine]
+  );
+
+  /* 포인터가 눌러진 체, 움직이면 추가해준다 */
+  const handlePointerMove = useCallback(
+    (e: React.PointerEvent<SVGSVGElement>) => {
+      const point = getPoint(e.clientX, e.clientY);
+
+      if (e.currentTarget.hasPointerCapture(e.pointerId)) {
+        addPointToLine(point);
+      }
+    },
+    [addPointToLine]
+  );
+
+  /* 포인터가 해제되었을 때, 완료해준다 */
+  const handlePointerUp = useCallback(
+    (e: React.PointerEvent<SVGSVGElement>) => {
+      e.currentTarget.releasePointerCapture(e.pointerId);
+
+      completeLine();
+    },
+    [completeLine]
+  );
+
+  return (
+    /* div */
+    <div
+      style={{
+        display: "inline-block",
+        width: "400px",
+        height: "400px",
+        border: "1px solid black",
+      }}
+    >
+      <svg
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
+        style={{
+          width: "400px",
+          height: "400px",
+        }}
+      >
+        <g>
+          {lines.map((line, i) => (
+            <Line key={line.get("id")} line={line} />
+          ))}
+        </g>
+      </svg>
+    </div>
   );
 }
