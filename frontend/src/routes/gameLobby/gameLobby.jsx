88cec73c1f3325ca138e209cfab004dc49127988
kimseungdeok <tmdejr1117@gmail.com>
2023-01-09 21:06:41 +0900

    Feat : WIP 채팅 스크롤 하단 고정 기능 작업중

--- frontend/src/routes/gameLobby/gameLobby.jsx
+++ frontend/src/routes/gameLobby/gameLobby.jsx
@@ -1,12 +1,69 @@
 import { Button, Container, Row, Col, InputGroup, Form } from "react-bootstrap"; // 꼭 import를 해와야한다
 import "./gameLobby.css";
 import Canvas from "../../components/canvas/canvas";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 
-function gameLobby() {
+const socket = io.connect("http://localhost:4000");
+
+function GameLobby() {
   const canvasSize = {
     width: "25rem",
     height: "90%",
-  } as React.CSSProperties;
+  };
+
+  const [state, setState] = useState({ message: "", name: "test1" });
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    // todo : 받을대는 new_message
+    // todo : 인수는 name, chat
+    socket.on("message", ({ name, message }) => {
+      setChat([...chat, { name, message }]);
+    });
+  });
+
+  const onTextChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const { name, message } = state;
+    // todo : 세션 스토리지에서 user id 가져오기
+    // todo : 보낼때는 submit_message로 보내기
+    
+    socket.emit("message", { name, message });
+    setState({ message: "", name });
+  };
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <li className="flex flex-row mb-2 border-gray-400" key={index}>
+        <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
+          <div className="flex-1 pl-1 md:mr-16">
+            <div className="font-medium">{name}</div>
+            <div className="flex">
+              <div className="text-xs text-gray-600 ">{message}</div>
+            </div>
+          </div>
+          <div className="text-xs text-gray-600 ">6:00 AM</div>
+        </div>
+      </li>
+    ));
+  };
+
+  let chatForm = document.querySelector(".chat-form");
+
+  function prepareScroll() {
+    window.setTimeout(scrollUl, 50);
+  }
+
+  function scrollUl() {
+    let chatUl = document.querySelector(".chat-ul");
+    chatUl.scrollTop = chatUl.scrollHeight;
+  }
+
+  chatForm.addEventListener("submit", prepareScroll);
 
   return (
     <div className="center">
@@ -116,96 +173,25 @@ function gameLobby() {
           </div>
           <div className="chat mx-auto rounded-md w-full">
             <div className="container flex flex-col items-center justify-center w-full mx-auto">
-              <ul className="flex flex-col overflow-auto chat-container w-full">
-                <li className="flex flex-row mb-2 border-gray-400">
-                  <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
-                    <div className="flex-1 pl-1 md:mr-16">
-                      <div className="font-medium">Jean Marc</div>
-                      <div className="flex">
-                        <div className="text-xs text-gray-600 ">
-                          채팅 메시지 입니다
-                        </div>
-                      </div>
-                    </div>
-                    <div className="text-xs text-gray-600 ">6:00 AM</div>
-                  </div>
-                </li>
-                <li className="flex flex-row mb-2 border-gray-400">
-                  <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
-                    <div className="flex-1 pl-1 md:mr-16">
-                      <div className="font-medium">Designer</div>
-                      <div className="flex">
-                        <div className="text-xs text-gray-600 ">
-                          채팅 메시지 입니다
-                        </div>
-                      </div>
-                    </div>
-                    <div className="text-xs text-gray-600 ">6:00 AM</div>
-                  </div>
-                </li>
-                <li className="flex flex-row mb-2 border-gray-400">
-                  <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
-                    <div className="flex-1 pl-1 md:mr-16">
-                      <div className="font-medium">Designer</div>
-                      <div className="flex">
-                        <div className="text-xs text-gray-600 ">
-                          채팅 메시지 입니다
-                        </div>
-                      </div>
-                    </div>
-                    <div className="text-xs text-gray-600 ">6:00 AM</div>
-                  </div>
-                </li>
-                <li className="flex flex-row mb-2 border-gray-400">
-                  <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
-                    <div className="flex-1 pl-1 md:mr-16">
-                      <div className="font-medium">Designer</div>
-                      <div className="flex">
-                        <div className="text-xs text-gray-600 ">
-                          채팅 메시지 입니다
-                        </div>
-                      </div>
-                    </div>
-                    <div className="text-xs text-gray-600 ">6:00 AM</div>
-                  </div>
-                </li>
-                <li className="flex flex-row mb-2 border-gray-400">
-                  <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
-                    <div className="flex-1 pl-1 md:mr-16">
-                      <div className="font-medium">CEO</div>
-                      <div className="flex">
-                        <div className="text-xs text-gray-600 ">
-                          채팅 메시지 입니다
-                        </div>
-                      </div>
-                    </div>
-                    <div className="text-xs text-gray-600 ">6:00 AM</div>
-                  </div>
-                </li>
-                <li className="flex flex-row mb-2 border-gray-400">
-                  <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
-                    <div className="flex-1 pl-1 md:mr-16">
-                      <div className="font-medium">CTO</div>
-                      <div className="flex">
-                        <div className="text-xs text-gray-600 ">
-                          채팅 메시지 입니다
-                        </div>
-                      </div>
-                    </div>
-                    <div className="text-xs text-gray-600 ">6:00 AM</div>
-                  </div>
-                </li>
+              <ul className="chat-ul  overflow-y-auto flex flex-col overflow-auto chat-container w-full">
+                {renderChat()}
               </ul>
 
               <div className="w-full rounded-md">
                 <div className="text-center font-light flex items-center justify-center">
-                  <form className="flex flex-col justify-center w-full max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
+                  <form
+                    className="chat-form flex flex-col justify-center w-full max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0"
+                    onSubmit={onMessageSubmit}
+                  >
                     <div className=" relative ">
                       <input
                         type="text"
                         id="form-subscribe-Subscribe"
-                        className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-12 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 pl-3 pr-20 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                         placeholder="채팅"
+                        name="message"
+                        onChange={(e) => onTextChange(e)}
+                        value={state.message}
                       />
                     </div>
                     <button
@@ -235,4 +221,4 @@ function gameLobby() {
   );
 }
 
-export default gameLobby;
+export default GameLobby;
