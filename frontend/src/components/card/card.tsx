9f4c3de002efd991dca712da2cb443b20b11990c
kimseungdeok <tmdejr1117@gmail.com>
2023-01-11 02:08:52 +0900

    Feat : Fix 카드 컴포넌트 수정
    컴포넌트의 재사용성을 높이기 위해 기존 컴포넌트 삭제 후 prop 추가

--- frontend/src/components/card/card.tsx
+++ frontend/src/components/card/card.tsx
@@ -1,21 +1,23 @@
 import { Button, Container, Row, Col } from "react-bootstrap"; // 꼭 import를 해와야한다
-import "./introCard.css";
+import "./card.css";
 
-function IntroCard({ title, imageSrc }: any) {
+function Card({ title, imageSrc, needTitle }: any) {
   return (
     <Container>
       <div className="flex items-center justify-center">
-        <div className="w-full p-3">
-          <div className="flex flex-col justify-center p-10 bg-white rounded-lg shadow-2xl card">
+        <div className="w-full p-1">
+          <div className="flex flex-col justify-center bg-white rounded-lg shadow-2xl card">
             <div className="prod-img">
               <img
                 src={imageSrc}
                 className="object-cover object-center w-full"
               />
             </div>
-            <p className="text-2xl font-bold text-gray-900 text-center uppercase">
-              {title}
-            </p>
+            {needTitle === true ? (
+              <p className="text-2xl font-bold text-gray-900 text-center uppercase">
+                {title}
+              </p>
+            ) : null}
           </div>
         </div>
       </div>
@@ -23,4 +25,4 @@ function IntroCard({ title, imageSrc }: any) {
   );
 }
 
-export default IntroCard;
+export default Card;
