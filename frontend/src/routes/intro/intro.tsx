b3e94dad94e9aca52400eee5815aed5ab1a8fe2e
kimseungdeok <tmdejr1117@gmail.com>
2023-01-11 02:10:53 +0900

    Feat : Add 초기 진입 페이지 생성

--- frontend/src/routes/intro/intro.tsx
+++ frontend/src/routes/intro/intro.tsx
@@ -1,6 +1,6 @@
 import { Button, Container, Row, Col } from "react-bootstrap"; // 꼭 import를 해와야한다
 import "./intro.css";
-import IntroCard from "../../components/introCard/introCard";
+import IntroCard from "../../components/card/card";
 
 // todo : 이미지 수정(타이틀에 맞게)
 function Intro() {
@@ -15,14 +15,17 @@ function Intro() {
       </div>
       <div className="flex flex-row">
         <IntroCard
+          needTitle={true}
           title="방 만들기"
           imageSrc="https://www.tailwind-kit.com/images/object/4.jpg"
         ></IntroCard>
         <IntroCard
+          needTitle={true}
           title="이어하기"
           imageSrc="https://www.tailwind-kit.com/images/object/4.jpg"
         ></IntroCard>
         <IntroCard
+          needTitle={true}
           title="방 들어가기"
           imageSrc="https://www.tailwind-kit.com/images/object/4.jpg"
         ></IntroCard>
