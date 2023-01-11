8ebe4d14bcb99ea22398421fdacbf49c7db91ef8
kimseungdeok <tmdejr1117@gmail.com>
2023-01-11 15:34:45 +0900

    Feat : Add 사용자 이름 추가

--- frontend/src/routes/intro/intro.tsx
+++ frontend/src/routes/intro/intro.tsx
@@ -13,7 +13,8 @@ function Intro() {
           언제, 어디서든 다함께 그림을 만들어보아요!
         </div>
         <div className="text-xl  mb-20">
-          <span className="text-purple-700">김승덕님</span> 환영합니다. 멀리있는 친구, 연인과 같이 그림을 완성해봐요.
+          <span className="text-purple-700">김승덕님</span> 환영합니다. 멀리있는
+          친구, 연인과 같이 그림을 완성해봐요.
         </div>
       </div>
       <div className="flex flex-row whitespace-nowrap h-[500px] overflow-auto">
