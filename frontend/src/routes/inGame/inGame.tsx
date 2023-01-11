139bae17075cdf6c4585aa71210902a5b45551fd
kimseungdeok <tmdejr1117@gmail.com>
2023-01-11 17:06:55 +0900

    Feat : Add 인게임 화면 완성

--- frontend/src/routes/inGame/inGame.tsx
+++ frontend/src/routes/inGame/inGame.tsx
@@ -4,12 +4,24 @@ import Canvas from "../../components/canvas/canvas";
 import UserState from "../../components/userState/userState";
 import InGameCanvas from "../../components/inGameCanvas/inGameCanvas";
 import SideNav from "../../components/sideNav/sideNav";
+import rightImage from "../../assets/cat-image.png";
+import leftImage from "../../assets/image_numbering_label.png";
 
 function InGame() {
   return (
-    <Container>
-      <SideNav/>
-    </Container>
+    <div className="ml-20">
+      <SideNav />
+      <div className=" h-screen">
+        <div className="grid grid-cols-2 w-full h-full">
+          <div className="flex flex-col justify-center border border-black h-full">
+            <img src={rightImage} alt="image"></img>
+          </div>
+          <div className="flex flex-col justify-center border border-black h-full">
+            <img src={leftImage} alt="image"></img>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
