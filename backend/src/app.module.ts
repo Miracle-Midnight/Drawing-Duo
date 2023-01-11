f6f7d298638d3ce5b73c3ebcd44b60b3c4eb16e5
young970 <yyww9@naver.com>
2023-01-11 22:20:26 +0900

    Fix: DBrds로 연결

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -38,7 +38,19 @@ import { MiddlewareConsumer, NestModule } from '@nestjs/common';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(dataSourceOptions),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: ['dist/src/*/entities/*.entity.{js,ts}'],
+
+      migrations: ['dist/db/migrations/*.{js.ts}'],
+
+      synchronize: true,
+    }),
     ChatsModule,
     DrawGatewayModule,
     ServeStaticModule.forRoot({
