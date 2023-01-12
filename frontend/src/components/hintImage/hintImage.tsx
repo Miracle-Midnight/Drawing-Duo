7d9024058ebb4c4c19d6d37b94e605142413e0dd
kimseungdeok <tmdejr1117@gmail.com>
2023-01-12 16:01:00 +0900

    Feat : Add 이미지 버튼 클릭시 이미지 오버레이 뜨는 기능 추가
    따로 컴포넌트로 나누어서 진행해야함

---
+++ frontend/src/components/hintImage/hintImage.tsx
@@ -0,0 +1,22 @@
+import catImage from "../../assets/cat-image.png";
+
+
+// todo : 힌트 이미지 컴포넌트 isHintImageOn props로 받아서 true일때만 보이게 하기
+function HintImage({isHintImageOn}:any) {
+  return 
+//   (
+//     {isHintImageOn === true ? (
+//         <div className="absolute bottom-48 left-20 z-50 border-2 border-gray-300 bg-opacity-50">
+//     <div className="w-[250px] p-3">
+//       <img src={catImage} alt="cat" />
+//     </div>
+//   </div>):(<div className="absolute bottom-48 left-20 z-50 border-2 border-gray-300 bg-opacity-50">
+//   <div className="w-[250px] p-3">
+//     <img src={catImage} alt="cat" />
+//   </div>
+// </div>)}
+    
+//   )
+}
+
+export default HintImage;
--- frontend/src/components/sideNav/sideNav.tsx
+++ frontend/src/components/sideNav/sideNav.tsx
@@ -2,20 +2,25 @@ import "./sideNav.css";
 import logoSmall from "../../assets/logo-small.png";
 import { useState } from "react";
 import InGamePlayer from "../inGamePlayer/inGamePlayer";
+import catImage from "../../assets/cat-image.png";
+import HintImage from "../hintImage/hintImage";
 
 function SideNav() {
   //   const [micState, setMicState] = useState(false);
   //   const [soundState, setSoundState] = useState(false);
   const [isMicOn, setisMicOn] = useState(false);
   const [isSoundOn, setisSoundOn] = useState(false);
+  const [isHintImageOn, setisHintImageOn] = useState(false);
 
   const toggleMicHandler = () => {
     // isMicOn의 상태를 변경하는 메소드를 구현
     setisMicOn(!isMicOn);
   };
   const toggleSoundHandler = () => {
-    // isMicOn의 상태를 변경하는 메소드를 구현
     setisSoundOn(!isSoundOn);
+  };
+  const toggleHintImageHandler = () => {
+    setisHintImageOn(!isHintImageOn);
   };
 
   return (
@@ -35,21 +40,39 @@ function SideNav() {
         <div className="mt-10 mb-10">
           <div className="side-nav mt-10">
             <ul>
-              <li className="my-12 text-center">
+              <li
+                className="my-12 text-center"
+                onClick={toggleHintImageHandler}
+              >
                 <a href="#">
                   <span className="h-6 w-6 text-gray-500  mx-auto hover:text-gray-800  transition-colors duration-200">
-                    <svg
-                      className="m-auto"
-                      fill="currentColor"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 32 32"
-                      version="1.1"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>image</title>
-                      <path d="M30 2.75h-28c-0.69 0-1.25 0.56-1.25 1.25v0 24c0 0.037 0.018 0.068 0.021 0.104 0.020 0.173 0.067 0.33 0.137 0.474l-0.004-0.009c0.040 0.074 0.083 0.137 0.132 0.196l-0.002-0.002c0.053 0.069 0.111 0.129 0.174 0.183l0.002 0.001c0.028 0.023 0.043 0.055 0.073 0.076 0.042 0.025 0.091 0.050 0.142 0.071l0.008 0.003c0.035 0.021 0.078 0.042 0.122 0.061l0.008 0.003c0.129 0.053 0.278 0.085 0.435 0.088l0.002-0 0 0h28c0.69-0.001 1.249-0.56 1.25-1.25v-24c-0-0.69-0.56-1.25-1.25-1.25h-0zM28.75 5.25v12.62l-5.709-8.563c-0.24-0.318-0.617-0.521-1.041-0.521s-0.801 0.203-1.039 0.518l-0.002 0.003-7.243 10.865-3.935-3.148c-0.212-0.17-0.484-0.273-0.781-0.273-0.422 0-0.796 0.209-1.022 0.529l-0.003 0.004-4.726 6.751v-18.784zM4.401 26.75l4.859-6.941 3.959 3.168c0.209 0.171 0.478 0.274 0.772 0.274 0.071 0 0.14-0.006 0.208-0.018l-0.007 0.001c0.356-0.056 0.656-0.256 0.846-0.537l0.003-0.004 6.96-10.439 6.75 10.126v4.37zM8 13.25c1.795 0 3.25-1.455 3.25-3.25s-1.455-3.25-3.25-3.25c-1.795 0-3.25 1.455-3.25 3.25v0c0.002 1.794 1.456 3.248 3.25 3.25h0zM8 9.25c0.414 0 0.75 0.336 0.75 0.75s-0.336 0.75-0.75 0.75c-0.414 0-0.75-0.336-0.75-0.75v0c0.001-0.414 0.336-0.749 0.75-0.75h0z"></path>
-                    </svg>
+                    {isHintImageOn === false ? (
+                      <svg
+                        className="m-auto"
+                        fill="currentColor"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 32 32"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <title>image</title>
+                        <path d="M30 2.75h-28c-0.69 0-1.25 0.56-1.25 1.25v0 24c0 0.037 0.018 0.068 0.021 0.104 0.020 0.173 0.067 0.33 0.137 0.474l-0.004-0.009c0.040 0.074 0.083 0.137 0.132 0.196l-0.002-0.002c0.053 0.069 0.111 0.129 0.174 0.183l0.002 0.001c0.028 0.023 0.043 0.055 0.073 0.076 0.042 0.025 0.091 0.050 0.142 0.071l0.008 0.003c0.035 0.021 0.078 0.042 0.122 0.061l0.008 0.003c0.129 0.053 0.278 0.085 0.435 0.088l0.002-0 0 0h28c0.69-0.001 1.249-0.56 1.25-1.25v-24c-0-0.69-0.56-1.25-1.25-1.25h-0zM28.75 5.25v12.62l-5.709-8.563c-0.24-0.318-0.617-0.521-1.041-0.521s-0.801 0.203-1.039 0.518l-0.002 0.003-7.243 10.865-3.935-3.148c-0.212-0.17-0.484-0.273-0.781-0.273-0.422 0-0.796 0.209-1.022 0.529l-0.003 0.004-4.726 6.751v-18.784zM4.401 26.75l4.859-6.941 3.959 3.168c0.209 0.171 0.478 0.274 0.772 0.274 0.071 0 0.14-0.006 0.208-0.018l-0.007 0.001c0.356-0.056 0.656-0.256 0.846-0.537l0.003-0.004 6.96-10.439 6.75 10.126v4.37zM8 13.25c1.795 0 3.25-1.455 3.25-3.25s-1.455-3.25-3.25-3.25c-1.795 0-3.25 1.455-3.25 3.25v0c0.002 1.794 1.456 3.248 3.25 3.25h0zM8 9.25c0.414 0 0.75 0.336 0.75 0.75s-0.336 0.75-0.75 0.75c-0.414 0-0.75-0.336-0.75-0.75v0c0.001-0.414 0.336-0.749 0.75-0.75h0z"></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        className="m-auto"
+                        fill="rgb(219,0,255)"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 32 32"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <title>image</title>
+                        <path d="M30 2.75h-28c-0.69 0-1.25 0.56-1.25 1.25v0 24c0 0.037 0.018 0.068 0.021 0.104 0.020 0.173 0.067 0.33 0.137 0.474l-0.004-0.009c0.040 0.074 0.083 0.137 0.132 0.196l-0.002-0.002c0.053 0.069 0.111 0.129 0.174 0.183l0.002 0.001c0.028 0.023 0.043 0.055 0.073 0.076 0.042 0.025 0.091 0.050 0.142 0.071l0.008 0.003c0.035 0.021 0.078 0.042 0.122 0.061l0.008 0.003c0.129 0.053 0.278 0.085 0.435 0.088l0.002-0 0 0h28c0.69-0.001 1.249-0.56 1.25-1.25v-24c-0-0.69-0.56-1.25-1.25-1.25h-0zM28.75 5.25v12.62l-5.709-8.563c-0.24-0.318-0.617-0.521-1.041-0.521s-0.801 0.203-1.039 0.518l-0.002 0.003-7.243 10.865-3.935-3.148c-0.212-0.17-0.484-0.273-0.781-0.273-0.422 0-0.796 0.209-1.022 0.529l-0.003 0.004-4.726 6.751v-18.784zM4.401 26.75l4.859-6.941 3.959 3.168c0.209 0.171 0.478 0.274 0.772 0.274 0.071 0 0.14-0.006 0.208-0.018l-0.007 0.001c0.356-0.056 0.656-0.256 0.846-0.537l0.003-0.004 6.96-10.439 6.75 10.126v4.37zM8 13.25c1.795 0 3.25-1.455 3.25-3.25s-1.455-3.25-3.25-3.25c-1.795 0-3.25 1.455-3.25 3.25v0c0.002 1.794 1.456 3.248 3.25 3.25h0zM8 9.25c0.414 0 0.75 0.336 0.75 0.75s-0.336 0.75-0.75 0.75c-0.414 0-0.75-0.336-0.75-0.75v0c0.001-0.414 0.336-0.749 0.75-0.75h0z"></path>
+                      </svg>
+                    )}
                   </span>
                 </a>
               </li>
@@ -197,6 +220,43 @@ function SideNav() {
           </div>
         </div>
       </nav>
+      <div className="absolute bottom-10 left-20 z-50 bg-gray-500 bg-opacity-50 hidden">
+        <div className="relative">
+          <div className="w-[350px] h-[400px] border-2 border-black">
+            <ul className="p-0 pt-3">
+              <li className="pl-3 pb-2">
+                <span>김영우</span>
+                <span className="px-1">:</span>
+                <span>sdfs</span>
+              </li>
+            </ul>
+            <div className="absolute bottom-0 w-full h-20px border-2 border-purple-500 ">
+              <form>
+                <label>
+                  <input type="text" name="name" />
+                </label>
+                <input type="submit" value="Submit" className="w-2/6" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      {isHintImageOn === true ? (
+        <div className="absolute bottom-48 left-20 z-50 border-2 border-gray-300 bg-opacity-50">
+          <div className="w-[250px] p-3">
+            <img src={catImage} alt="cat" />
+          </div>
+        </div>
+      ) : (
+        <div className="absolute bottom-48 left-20 z-50 border-2 border-gray-300 bg-opacity-50 hidden">
+          <div className="w-[250px] p-3">
+            <img src={catImage} alt="cat" />
+          </div>
+        </div>
+      )}
+
+      {/* <HintImage isHintImageOn={isHintImageOn}></HintImage> */}
     </div>
   );
 }
