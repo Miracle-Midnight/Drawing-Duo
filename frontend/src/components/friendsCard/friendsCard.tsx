d80de95d9b8634b04dacf939bb2e055d61ac1da6
kimseungdeok <tmdejr1117@gmail.com>
2023-01-12 23:07:07 +0900

    Feat : 초대 받은 사람에게만 수락 거절 버튼 나오도록 수정

--- frontend/src/components/friendsCard/friendsCard.tsx
+++ frontend/src/components/friendsCard/friendsCard.tsx
@@ -1,4 +1,4 @@
-function FriendsCard({ name, isConnected }: any) {
+function FriendsCard({ name, isConnected, isInvited }: any) {
   return (
     <li className="flex flex-row mb-2 border-gray-400">
       <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white  rounded-md flex flex-1 items-center p-4">
@@ -11,14 +11,17 @@ function FriendsCard({ name, isConnected }: any) {
           )}
         </div>
         <div className="text-xs text-gray-600 ">
-          <div>
-            <button className="bg-green-300 hover:bg-green-600 text-gray-800 hover:text-white font-bold py-2 px-4 rounded inline-flex items-center mr-1">
-              <span>수락</span>
-            </button>
-            <button className="bg-red-300 hover:bg-red-600 text-gray-800 hover:text-white font-bold py-2 px-4 rounded inline-flex items-center">
-              <span>거절</span>
-            </button>
-          </div>
+          {isInvited === true ? (
+            <div>
+              {/* todo : 수락, 거절 버튼 svg로 만들기 */}
+              <button className="bg-green-300 hover:bg-green-600 text-gray-800 hover:text-white font-bold py-2 px-4 rounded inline-flex items-center mr-1">
+                <span>수락</span>
+              </button>
+              <button className="bg-red-300 hover:bg-red-600 text-gray-800 hover:text-white font-bold py-2 px-4 rounded inline-flex items-center">
+                <span>거절</span>
+              </button>
+            </div>
+          ) : null}
         </div>
       </div>
     </li>
