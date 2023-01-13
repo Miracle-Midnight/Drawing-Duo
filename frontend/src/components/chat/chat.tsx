479e7372c500654b533e6e681df485f4bec0f306
kimseungdeok <tmdejr1117@gmail.com>
2023-01-14 01:40:46 +0900

    Feat : Add 채팅 왼쪽 오른쪽 구분
    본인이 한 채팅은 오른쪽 상대방이 한 채팅은 왼쪽에 배치

--- frontend/src/components/chat/chat.tsx
+++ frontend/src/components/chat/chat.tsx
@@ -1,12 +1,20 @@
-function Chat({ name="기본값", message }: any) {
-  return (
-    <>
-      <li className="pl-3 pb-2">
-        <span>{name}</span>
-        <span className="px-1">:</span>
-        <span>{message}</span>
-      </li>
-    </>
+function Chat({ name = "기본값", message, isYou }: any) {
+  return isYou === false ? (
+    <li className="pl-3 pb-2 justify-end flex flex-col justify-end">
+      <div className="flex justify-end">
+        <div>{name}</div>
+
+        <div>{message}</div>
+      </div>
+    </li>
+  ) : (
+    <li className="pl-3 pb-2 flex justify-end">
+      <div className="flex flex-col ">
+        <div>테스트</div>
+
+        <div className="border-2 border-gray-100 p-2 px-4 rounded-tl-xl rounded-tr-xl rounded-bl-xl bg-purple-300 text-gray-700">{message}</div>
+      </div>
+    </li>
   );
 }
 
