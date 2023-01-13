170096ea48a8e74b8dbd7b7a81269f36773d1306
kimseungdeok <tmdejr1117@gmail.com>
2023-01-14 03:17:43 +0900

    Feat : Fix 왼쪽과 오른쪽 디자인 변경

--- frontend/src/components/chat/chat.tsx
+++ frontend/src/components/chat/chat.tsx
@@ -1,18 +1,22 @@
 function Chat({ name = "기본값", message, isYou }: any) {
   return isYou === false ? (
-    <li className="pl-3 pb-2 justify-end flex flex-col justify-end">
-      <div className="flex justify-end">
+    <li className="pl-3 pb-2 flex justify-start">
+      <div className="flex flex-col ">
         <div>{name}</div>
 
-        <div>{message}</div>
+        <div className="border-2 border-gray-100 p-2 px-4 rounded-tl-xl rounded-tr-xl rounded-br-xl bg-purple-300 text-gray-700">
+          {message}
+        </div>
       </div>
     </li>
   ) : (
     <li className="pl-3 pb-2 flex justify-end">
       <div className="flex flex-col ">
-        <div>테스트</div>
+        <div>{name}</div>
 
-        <div className="border-2 border-gray-100 p-2 px-4 rounded-tl-xl rounded-tr-xl rounded-bl-xl bg-purple-300 text-gray-700">{message}</div>
+        <div className="border-2 border-gray-100 p-2 px-4 rounded-tl-xl rounded-tr-xl rounded-bl-xl bg-purple-300 text-gray-700">
+          {message}
+        </div>
       </div>
     </li>
   );
