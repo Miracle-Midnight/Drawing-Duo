2b005dc6985ccc3f81988548e5387cbe7354f72d
YoungwooKim09 <youngwoo9699@gmail.com>
2023-01-15 05:06:43 +0900

    Fix: Modify TypeOrmModule in app.module

--- backend/db/data-source.ts
+++ backend/db/data-source.ts
@@ -2,16 +2,13 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'drawingduo',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['dist/src/*/entities/*.entity.{js,ts}'],
-
   migrations: ['dist/db/migrations/*.{js.ts}'],
-
-  synchronize: true,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
