34fb91bb8f43861b29a36e963e56cd18e821875d
kimseungdeok <tmdejr1117@gmail.com>
2023-01-21 00:14:53 +0900

    Feat : Add drawingTools가 마우스에 따라 움직이도록 설정
    마우스 클릭시에만 움직이도록 바꾸어야함

--- frontend/src/components/drawTools/drawTools.tsx
+++ frontend/src/components/drawTools/drawTools.tsx
@@ -1,5 +1,5 @@
 /* library */
-import React from "react";
+import React, { useState, useEffect } from "react";
 /* module from local */
 import Pen from "./pen";
 import Eraser from "./eraser";
@@ -21,8 +21,47 @@ export function DrawTools() {
     redoLine,
   } = useLines();
 
+  const [position, setPosition] = useState({ x: 200, y: 100 });
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  useEffect(() => {
+    const drawingTools = document.getElementById("drawingTools");
+    if (drawingTools) {
+      drawingTools.addEventListener("mousedown", () => {
+        setIsMouseDown(true);
+      });
+      drawingTools.addEventListener("mouseup", () => {
+        setIsMouseDown(false);
+      });
+
+      drawingTools.addEventListener("mousemove", (event: MouseEvent) => {
+        if (isMouseDown) {
+          setPosition({ x: event.clientX, y: event.clientY });
+        }
+      });
+      return () => {
+        drawingTools.removeEventListener("mousedown", () => {
+          setIsMouseDown(true);
+        });
+        drawingTools.removeEventListener("mouseup", () => {
+          setIsMouseDown(false);
+        });
+
+        drawingTools.removeEventListener("mousemove", (event: MouseEvent) => {
+          if (isMouseDown) {
+            setPosition({ x: event.clientX, y: event.clientY });
+          }
+        });
+      };
+    }
+  }, [isMouseDown]);
+
   return (
-    <div className="flex flex-row">
+    <div
+      id="drawingTools"
+      style={{ position: "absolute", left: position.x, top: position.y }}
+      className="flex flex-row"
+    >
       <Pen></Pen>
       <Eraser></Eraser>
       <InputRange min={1} max={100}></InputRange>
