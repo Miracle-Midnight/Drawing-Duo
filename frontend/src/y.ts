6c4db3e4c34aea0a3b54f6af280e591082443cab
kimseungdeok <tmdejr1117@gmail.com>
2023-01-20 16:52:32 +0900

    Fix : password '' 으로 설정
    계속 만들지만 사라져버리는 password를 다시 생성

--- frontend/src/y.ts
+++ frontend/src/y.ts
@@ -14,7 +14,7 @@ export const doc = new Y.Doc();
 export const provider = new WebrtcProvider(roomId, doc, {
   // Specify signaling servers. The client will connect to every signaling server concurrently to find other peers as fast as possible.
   signaling: ["ws://54.180.118.157:4000"],
-
+  password: '',
   awareness: new awarenessProtocol.Awareness(doc),
   maxConns: 20,
   filterBcConns: true,
