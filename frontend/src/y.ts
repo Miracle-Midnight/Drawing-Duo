d5f2520d7de8b61d29f14632fc7d56f2ab1ce0d7
kimseungdeok <tmdejr1117@gmail.com>
2023-01-22 03:31:23 +0900

    Feat : WIP  중간 저장 기능 하는 중

--- frontend/src/y.ts
+++ frontend/src/y.ts
@@ -4,6 +4,7 @@ import { WebrtcProvider } from "y-webrtc";
 import { IndexeddbPersistence } from "y-indexeddb";
 import { USER_COLORS } from "./constants";
 import * as awarenessProtocol from "y-protocols/awareness";
+import axios from "axios";
 
 const roomId: string = sessionStorage.getItem("roomId")!;
 
@@ -33,6 +34,14 @@ export const awareness = provider.awareness;
 
 /* 공유 데이터를 담는 YArray 생성, doc에서 lines라는 Y배열을 가진 clinet끼리 공유*/
 export const yLines: Y.Array<Y.Map<any>> = doc.getArray("lines");
+// setInterval(() => console.log(yLines), 2000);
+
+// axios
+//   .post("/", yLines.toJSON())
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.log(err));
 
 /* 취소 기능을 위해 manager 생성 */
 export const undoManager = new Y.UndoManager(yLines);
